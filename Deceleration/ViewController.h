eb58647f167aefa2603dc45206dbe8b22ced0028
rajendrahn <rajendra.hn@robosoftin.com>
2013-02-04 10:15:42 +0530

    Initial Commit

---
+++ Deceleration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Deceleration
+//
+//  Created by Rajendra HN on 04/02/13.
+//  Copyright (c) 2013 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Deceleration/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Deceleration
+//
+//  Created by Rajendra HN on 04/02/13.
+//  Copyright (c) 2013 Robosoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
